eb128dc2914f417453c6c35390d427fa20ce43a9
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-07-08 17:16:35 -0400

    feat: Add the starting version of the configure module.

---
+++ actionstory/configure.py
@@ -0,0 +1,29 @@
+"""Configure logging and console output."""
+
+import logging
+import logging.config
+
+from rich.logging import RichHandler
+from rich.traceback import install
+
+from actionstory import constants
+
+
+def configure_tracebacks() -> None:
+    """Configure stack tracebacks arising from a crash to use rich."""
+    install()
+
+
+def configure_logging(
+    debug_level: str = constants.logging.Default_Logging_Level,
+) -> logging.Logger:
+    """Configure standard Python logging package to use rich."""
+    logging.basicConfig(
+        level=debug_level,
+        format=constants.logging.Format,
+        datefmt="[%X]",
+        handlers=[RichHandler()],
+    )
+    # create a global logger and then make it available with the "Rich" name
+    logger = logging.getLogger(constants.logging.Rich)
+    return logger
