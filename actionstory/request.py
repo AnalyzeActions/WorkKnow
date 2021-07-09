5e21be9136b34b973ebbb36af77ad6925659e7c9
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-07-08 21:40:15 -0400

    feat: Add start to the feature in request that uses GitHub pagination.

--- actionstory/request.py
+++ actionstory/request.py
@@ -1,8 +1,13 @@
 """Use Python HTTPx to Access the GitHub API."""
 
 import logging
+import os
+
+from typing import Dict
+from typing import List
 
 import requests
+from dotenv import load_dotenv
 
 from actionstory import constants
 
@@ -19,15 +24,28 @@ from actionstory import constants
 # │   ]
 # }
 
+load_dotenv()
 
-def request_json_from_github(github_api_url: str) -> str:
+
+def get_github_personal_access_token():
+    """Retrieve the GitHub personal access token from the environment."""
+    github_personal_access_token = os.getenv(constants.environment.Github)
+    return github_personal_access_token
+
+
+def request_json_from_github(github_api_url: str) -> Dict[str, Dict[str, List[str]]]:
     """Request the JSON response from the GitHub API."""
-    github_params = {"per_page": "100"}
-    response = requests.get(github_api_url, headers=github_params)
-    json_response = response.json()
     logger = logging.getLogger(constants.logging.Rich)
+    github_authentication = ('user', get_github_personal_access_token())
+    github_params = {"per_page": "30"}
+    response = requests.get(github_api_url, auth=github_authentication)
+    json_responses = response.json()
     logger.debug(response.headers)
+    page = 2
     while "next" in response.links.keys():
-        response = requests.get(response.links["next"]["url"])
-        json_response.update(response.json())
-    return json_response
+        github_params = {"page": str(page)}
+        response = requests.get(github_api_url, params=github_params, auth=github_authentication)
+        logger.debug(response.headers)
+        json_responses.update(response.json())
+        page = page + 1
+    return json_responses
