ce6e6c3487a5f81723eba07f734de8a175acc1f5
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-07-10 15:24:48 -0400

    refactor: Move the creation of the directory to first step in files.

--- workknow/files.py
+++ workknow/files.py
@@ -29,6 +29,10 @@ def save_dataframe(
     results_dir: Path, organization: str, repository: str, repo_data: pandas.DataFrame
 ) -> None:
     """Save the provided DataFrame in a file connected to organization and repo in the results_dir."""
+    # create the complete file path, making all parent directories
+    # if needed and not failing if the directory already exists
+    create_directory(results_dir)
+    results_dir.mkdir(parents=True, exist_ok=True)
     # create the directory given the provided input details
     file_name = organization + "-" + repository + ".csv"
     # log the name of the file and the results directory
@@ -39,9 +43,6 @@ def save_dataframe(
     complete_file_path = results_dir / file_name
     # resolve the complete file path to get its absolute name
     resolved_complete_file_path = complete_file_path.resolve()
-    # create the complete file path, making all parent directories
-    # if needed and not failing if the directory already exists
-    results_dir.mkdir(parents=True, exist_ok=True)
     # convert the pathlib Path object to a string and then use
     # Pandas to save the file to the textualized path as a CSV file
     repo_data.to_csv(str(resolved_complete_file_path))
