a3565c9d86af019bda9812abbb931ba25e3db47b
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-07-10 15:11:47 -0400

    coms: Add comments to the save_dataframe function in the files module.

--- workknow/files.py
+++ workknow/files.py
@@ -4,6 +4,7 @@ import logging
 from pathlib import Path
 
 import pandas
+import typer
 
 from workknow import constants
 
@@ -13,10 +14,18 @@ def save_dataframe(
 ) -> None:
     """Save the provided DataFrame in a file connected to organization and repo in the results_dir."""
     # create the directory given the provided input details
-    logger = logging.getLogger(constants.logging.Rich)
     file_name = organization + "-" + repository + ".csv"
+    # log the name of the file and the results directory
+    logger = logging.getLogger(constants.logging.Rich)
     logger.debug(results_dir)
+    logger.debug(file_name)
+    # construct the complete file path
     complete_file_path = results_dir / file_name
+    # resolve the complete file path to get its absolute name
     resolved_complete_file_path = complete_file_path.resolve()
+    # create the complete file path, making all parent directories
+    # if needed and not failing if the directory already exists
     results_dir.mkdir(parents=True, exist_ok=True)
+    # convert the pathlib Path object to a string and then use
+    # Pandas to save the file to the textualized path as a CSV file
     repo_data.to_csv(str(resolved_complete_file_path))
