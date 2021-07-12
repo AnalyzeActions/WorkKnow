15e6fb42d693c4b70e1204ff9deff3624c448946
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-07-12 12:03:45 -0400

    feat: Improve the command-line interface and output in main.

--- workknow/main.py
+++ workknow/main.py
@@ -46,8 +46,7 @@ def setup(debug_level: DebugLevel) -> Tuple[Console, Logger]:
 
 @cli.command()
 def analyze(
-    organization: str = typer.Option(...),
-    repo: str = typer.Option(...),
+    repo_url: str = typer.Option(...),
     debug_level: DebugLevel = DebugLevel.ERROR,
     results_dir: Path = typer.Option(None),
     save: bool = typer.Option(False),
@@ -60,57 +59,61 @@ def analyze(
     console.print(
         constants.workknow.Emoji + constants.markers.Space + constants.workknow.Tagline
     )
+    console.print(
+        constants.workknow.Website
+    )
     console.print()
     # STEP: create the URL needed for accessing the repository's Action builds
-    github_api_url = produce.create_github_api_url(organization, repo)
-    console.print("Analyzing the build history of the GitHub repository at:")
-    console.print(github_api_url, style="link " + github_api_url)
-    console.print()
-    # STEP: access the JSON file that contains the build history
-    json_responses = request.request_json_from_github(github_api_url)
-    console.print(
-        f"Downloaded a total of {produce.count_individual_builds(json_responses)} records that look like:"
-    )
-    # STEP: print debugging information in a summarized fashion
-    pprint(json_responses, max_length=3)
-    console.print()
-    console.print("The first workflow record looks like:")
-    pprint(json_responses[0][0], max_length=25)
-    logger.debug(json_responses[0][0])
-    console.print()
-    pprint(json_responses[0][0])
-    # STEP: create the workflows DataFrame
-    workflows_dataframe = produce.create_workflows_dataframe(json_responses)
-    # STEP: create the commit details DataFrame
-    commits_dataframe = produce.create_commits_dataframe(json_responses)
-    pprint(commits_dataframe)
-    # STEP: save the workflows DataFrame when saving is stipulated and
-    # the results directory is valid for the user's file system
-    if save and files.confirm_valid_directory(results_dir):
-        # save the workflows DataFrame
+    organization, repo = produce.parse_github_url(repo_url)
+    if organization is not None and repo is not None:
+        github_api_url = produce.create_github_api_url(organization, repo)
+        console.print("Analyzing the workflow history of the GitHub repository at:")
+        console.print(github_api_url, style="link " + github_api_url)
+        console.print()
+        # STEP: access the JSON file that contains the build history
+        json_responses = request.request_json_from_github(github_api_url)
         console.print(
-            f"Saving workflow details for {organization}/{repo} in the directory {str(results_dir).strip()}"
+            f":inbox_tray: Downloaded a total of {produce.count_individual_builds(json_responses)} records that look like:\n"
         )
-        files.save_dataframe(
-            results_dir,
-            organization,
-            repo,
-            constants.filesystem.Workflows,
-            workflows_dataframe,
-        )
-        # save the commits DataFrame
-        console.print(
-            f"Saving commit details for {organization}/{repo} in the directory {str(results_dir).strip()}"
-        )
-        files.save_dataframe(
-            results_dir,
-            organization,
-            repo,
-            constants.filesystem.Commits,
-            commits_dataframe,
-        )
-    else:
-        # explain that the save could not work correctly due to invalid results directory
-        console.print(
-            f"Could not save workflow and commit details for {organization}/{repo} in the directory {str(results_dir).strip()}"
-        )
+        # STEP: print debugging information in a summarized fashion
+        pprint(json_responses, max_length=3)
+        console.print()
+        console.print(":lion_face: The first workflow record looks like:\n")
+        pprint(json_responses[0][0], max_length=25)
+        logger.debug(json_responses[0][0])
+        console.print()
+        # pprint(json_responses[0][0])
+        # STEP: create the workflows DataFrame
+        workflows_dataframe = produce.create_workflows_dataframe(json_responses)
+        # STEP: create the commit details DataFrame
+        commits_dataframe = produce.create_commits_dataframe(json_responses)
+        # STEP: save the workflows DataFrame when saving is stipulated and
+        # the results directory is valid for the user's file system
+        if save and files.confirm_valid_directory(results_dir):
+            # save the workflows DataFrame
+            console.print(
+                f":sparkles: Saving details for {organization}/{repo} in the directory {str(results_dir).strip()}"
+            )
+            console.print("\t... Save the workflow file.")
+            files.save_dataframe(
+                results_dir,
+                organization,
+                repo,
+                constants.filesystem.Workflows,
+                workflows_dataframe,
+            )
+            # save the commits DataFrame
+            console.print("\t... Save the commits file.")
+            files.save_dataframe(
+                results_dir,
+                organization,
+                repo,
+                constants.filesystem.Commits,
+                commits_dataframe,
+            )
+        else:
+            # explain that the save could not work correctly due to invalid results directory
+            console.print(
+                f"Could not save workflow and commit details for {organization}/{repo} in the directory {str(results_dir).strip()}"
+            )
+        console.print()
