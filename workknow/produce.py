5b9a42d8f56a83117b0a6aeeb1f12cd25239429d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-07-12 11:03:34 -0400

    feat: Add functions to produce to generalize and extract commit data.

--- workknow/produce.py
+++ workknow/produce.py
@@ -5,6 +5,7 @@ import logging
 from typing import Any
 from typing import Dict
 from typing import List
+from typing import Set
 
 import pandas
 
@@ -41,23 +42,10 @@ def count_individual_builds(json_responses: List[Dict[Any, Any]]) -> int:
     return running_build_total
 
 
-def create_workflows_dataframe(
-    workflows_dictionary_list: List[Dict[Any, Any]]
+def create_subsetted_dataframe(
+    subset_key_names: Set, workflows_dictionary_list: List[Dict[Any, Any]]
 ) -> pandas.DataFrame:
     """Create a DataFrame of all of the relevant workflow data."""
-    # create a tuple of the key names that we want to retain from
-    # those keys that are inside of all those in a dictionary (row) of data
-    subset_key_names = {
-        constants.workflow.Id,
-        constants.workflow.Name,
-        constants.workflow.Head_Sha,
-        constants.workflow.Created_At,
-        constants.workflow.Updated_At,
-        constants.workflow.Event,
-        constants.workflow.Status,
-        constants.workflow.Conclusion,
-        constants.workflow.Jobs_Url,
-    }
     # create an empty list that will store dictionaries to be made into
     # rows of a Pandas DataFrame. This approach avoids the need to incrementally
     # add rows to a Pandas DataFrame, which is known to be inefficient.
@@ -76,6 +64,45 @@ def create_workflows_dataframe(
             }
             # add the list of chosen key-value pairs to the list of workflow details
             total_workflow_list.append(chosen_keys_values)
+    return total_workflow_list
     # create a Pandas DataFrame from the list of dictionaries
+    # total_workflow_dataframe = pandas.DataFrame(total_workflow_list)
+    # return total_workflow_dataframe
+
+
+def create_workflows_dataframe(
+    workflows_dictionary_list: List[Dict[Any, Any]]
+) -> pandas.DataFrame:
+    """Create a DataFrame of all of the relevant workflow data."""
+    # create a tuple of the key names that we want to retain from
+    # those keys that are inside of all those in a dictionary (row) of data
+    subset_key_names = {
+        constants.workflow.Id,
+        constants.workflow.Name,
+        constants.workflow.Head_Sha,
+        constants.workflow.Created_At,
+        constants.workflow.Updated_At,
+        constants.workflow.Event,
+        constants.workflow.Status,
+        constants.workflow.Conclusion,
+        constants.workflow.Jobs_Url,
+    }
+    total_workflow_list = create_subsetted_dataframe(
+        subset_key_names, workflows_dictionary_list
+    )
     total_workflow_dataframe = pandas.DataFrame(total_workflow_list)
     return total_workflow_dataframe
+
+
+def create_commits_dataframe(
+    workflows_dictionary_list: List[Dict[Any, Any]]
+) -> pandas.DataFrame:
+    """Create a DataFrame of all the relevant commit message data."""
+    # create a tuple of the key names that we want to retain from
+    # those keys that are inside of all those in a dictionary (row) of data
+    subset_key_names = {
+        constants.workflow.Head_Commit,
+    }
+    commits_dataframe = create_subsetted_dataframe(subset_key_names, workflows_dictionary_list)
+    total_commits_dataframe = pandas.json_normalize(commits_dataframe, sep="_")
+    return total_commits_dataframe
