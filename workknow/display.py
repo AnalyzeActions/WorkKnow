71e8f59f4df783db0e4bb381f03d44da9c02734a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-07-12 21:17:13 -0400

    fix: Use the correct imports and function call in display_tool_details.

--- workknow/display.py
+++ workknow/display.py
@@ -1,8 +1,8 @@
 """Display messages in the terminal window."""
 
+from workknow import configure
 from workknow import constants
 from workknow import debug
-from workknow import main
 
 
 def display_tool_details(
@@ -10,7 +10,7 @@ def display_tool_details(
 ):
     """Display the details about the tool."""
     # STEP: setup the console and the logger and then create a blank line for space
-    console, logger = main.setup(debug_level)
+    console, logger = configure.setup(debug_level)
     # STEP: display the messages about the tool
     console.print()
     console.print(
