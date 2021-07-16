f613b6e386dd37baabcea0804b50d67046b814b5
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-07-16 12:10:07 -0400

    feat: Add suitable error handling for wrong semver in release module.

--- workknow/release.py
+++ workknow/release.py
@@ -3,6 +3,7 @@
 from pathlib import Path
 
 import logging
+import sys
 
 from rich.progress import BarColumn
 from rich.progress import Progress
@@ -13,8 +14,7 @@ from github import Github
 from github import GithubException
 from github import InputGitAuthor
 
-from github_binary_upload import publish_release_from_tag
-
+from workknow import configure
 from workknow import constants
 from workknow import request
 
@@ -25,6 +25,8 @@ def perform_github_upload(
     """Create a new release on GitHub of all files in the results directory."""
     # create a logger
     logger = logging.getLogger(constants.logging.Rich)
+    # get a console for use in diagnostic display
+    console = configure.setup_console()
     # extract the github_access_token for use during upload process
     github_access_token = request.get_github_personal_access_token()
     github = Github(github_access_token)
@@ -89,7 +91,6 @@ def perform_github_upload(
                 )
                 logger.debug(result_file_name + " UPDATED")
                 commit_sha = update_dict["commit"].sha
-                print(commit_sha)
             else:
                 create_dict = github_repository.create_file(
                     result_file_name,
@@ -97,12 +98,41 @@ def perform_github_upload(
                     results_files_contents[result_file_name],
                 )
                 commit_sha = create_dict["commit"].sha
-                print(commit_sha)
                 logger.debug(result_file_name + " CREATED")
             progress.update(upload_pages_task, advance=1)
-    input_git_author = InputGitAuthor("Gregory M. Kapfhammer", "gkapfham@allegheny.edu", "2014-11-07T22:01:45Z")
-    github_repository.create_git_tag_and_release("v0.1.0", "Tag Message", "Release Name", "Release Message", str(commit_sha), "Type", input_git_author, False, False)
-    # publish_release_from_tag("AnalyzeActions/WorkKnow-Data", )
+    # create a GitHub author for use in creating the tagged release of the repository
+    # note that the date for the InputGitAuthor was extracted from this web site:
+    # https://docs.github.com/en/rest/reference/git#tags
+    # since it would ensure that the datetime was in the required format
+    input_git_author = InputGitAuthor(
+        "Gregory M. Kapfhammer", "gkapfham@allegheny.edu", "2014-11-07T22:01:45Z"
+    )
+    # create a GitHub release that will produce a .zip and .tar.gz file of all the
+    # contents of the repository at the specific tag of the provided semver
+    try:
+        github_repository.create_git_tag_and_release(
+            semver,
+            "WorkKnow Default Tag Message",
+            "WorkKnow Data Release " + semver,
+            "WorkKnow automatically released data at version " + semver,
+            str(commit_sha),
+            "Type",
+            input_git_author,
+            False,
+            False,
+        )
+    except GithubException:
+        console.print()
+        console.print(":grimacing_face: Unable to create a tagged release through the GitHub API")
+        console.print(
+            "Did you use a unique semantic version number (i.e., semver) for the release?"
+            + constants.markers.Newline
+            + constants.markers.Newline
+            + ":sad_but_relieved_face: Exiting now!"
+        )
+        console.print()
+        sys.exit(1)
+
 
 
 def get_blob_content(repo, branch, path_name):
