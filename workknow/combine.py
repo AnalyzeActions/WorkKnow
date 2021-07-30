a705bd9f35204af9944a87774eac8052dee3b9cc
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-07-30 15:21:17 -0400

    feat: Add progress bars to both create steps in combine module.

--- workknow/combine.py
+++ workknow/combine.py
@@ -9,6 +9,12 @@ from typing import Tuple
 
 import pandas
 
+from rich.progress import BarColumn
+from rich.progress import Progress
+from rich.progress import TimeRemainingColumn
+from rich.progress import TimeElapsedColumn
+
+from workknow import configure
 from workknow import constants
 
 
@@ -17,23 +23,71 @@ def summarize_files_in_directory(
 ) -> Tuple[pandas.DataFrame, pandas.DataFrame]:
     """Summarize all of the CSV files inside of a directory."""
     logger = logging.getLogger(constants.logging.Rich)
+    console = configure.setup_console()
     data_frame_list_commits: List[pandas.DataFrame] = []
     data_frame_list_workflows: List[pandas.DataFrame] = []
+    commits_data_frame = None
     # extract all of the commits-based CSV files
-    for csv_file in sorted(csv_directory.glob(constants.filesystem.Csv_Commits_Glob)):
-        logger.debug(csv_file)
-        csv_file_data_frame = pandas.read_csv(str(csv_file))
-        data_frame_list_commits.append(csv_file_data_frame)
+    with Progress(
+        constants.progress.Task_Format,
+        BarColumn(),
+        constants.progress.Percentage_Format,
+        constants.progress.Completed,
+        "•",
+        TimeElapsedColumn(),
+        "elapsed",
+        "•",
+        TimeRemainingColumn(),
+        "remaining",
+    ) as progress:
+        sorted_directory_glob = sorted(
+            csv_directory.glob(constants.filesystem.Csv_Commits_Glob)
+        )
+        task = progress.add_task(
+            "Combine Commit Data", total=len(sorted_directory_glob) + 1
+        )
+        for csv_file in sorted_directory_glob:
+            logger.debug(csv_file)
+            csv_file_data_frame = pandas.read_csv(str(csv_file))
+            data_frame_list_commits.append(csv_file_data_frame)
+            progress.update(task, advance=1)
+        commits_data_frame = pandas.concat(data_frame_list_commits)
+        progress.update(task, advance=1)
     logger.debug(len(data_frame_list_commits))
     # extract all of the workflow-based CSV files
-    for csv_file in sorted(csv_directory.glob(constants.filesystem.Csv_Workflows_Glob)):
-        logger.debug(csv_file)
-        csv_file_data_frame = pandas.read_csv(str(csv_file))
-        data_frame_list_workflows.append(csv_file_data_frame)
+    console.print()
+    with Progress(
+        constants.progress.Task_Format,
+        BarColumn(),
+        constants.progress.Percentage_Format,
+        constants.progress.Completed,
+        "•",
+        TimeElapsedColumn(),
+        "elapsed",
+        "•",
+        TimeRemainingColumn(),
+        "remaining",
+    ) as progress:
+        sorted_directory_glob = sorted(
+            csv_directory.glob(constants.filesystem.Csv_Workflows_Glob)
+        )
+        task = progress.add_task(
+            "Combine Workflow Data", total=len(sorted_directory_glob) + 1
+        )
+        for csv_file in sorted(
+            csv_directory.glob(constants.filesystem.Csv_Workflows_Glob)
+        ):
+            logger.debug(csv_file)
+            csv_file_data_frame = pandas.read_csv(str(csv_file))
+            data_frame_list_workflows.append(csv_file_data_frame)
+            progress.update(task, advance=1)
+        workflows_data_frame = pandas.concat(data_frame_list_workflows)
+        progress.update(task, advance=1)
     logger.debug(len(data_frame_list_workflows))
+    console.print()
     return (
-        pandas.concat(data_frame_list_commits),
-        pandas.concat(data_frame_list_workflows),
+        commits_data_frame,
+        workflows_data_frame,
     )
 
 
