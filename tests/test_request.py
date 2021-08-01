1b0279d92d064d432f662c8abf7066a05da7e622
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-07-31 23:04:36 -0400

    test: Add the starting version of test_request for backup mechanism.

---
+++ tests/test_request.py
@@ -0,0 +1,24 @@
+"""Tests for the request module."""
+
+import responses
+
+from workknow import configure
+from workknow import debug
+from workknow import request
+
+
+@responses.activate
+def test_simple():
+    """Check that a mocked request to an API fails and handles correctly."""
+    debug_level = debug.DebugLevel.ERROR
+    console, _ = configure.setup(debug_level)
+    github_api_url = "https://api.github.com/repos/home-assistant/core/actions/runs"
+    responses.add(
+        responses.GET,
+        github_api_url,
+        json={"message": "server error"},
+        status=502,
+    )
+    (valid, json_responses) = request.request_json_from_github(github_api_url, console)
+    assert valid is False
+    assert json_responses == {"message": "server error"}
