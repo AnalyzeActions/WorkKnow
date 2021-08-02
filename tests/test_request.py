82c9b1af022f1c4985e01e75bc621e95db4b9769
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-08-02 11:48:15 -0400

    test: Fix test case name and add comments to test in test_request.

--- tests/test_request.py
+++ tests/test_request.py
@@ -8,17 +8,25 @@ from workknow import request
 
 
 @responses.activate
-def test_simple():
+def test_failure_backoff_three_retries_mocked():
     """Check that a mocked request to an API fails and handles correctly."""
+    # configure the system so that it does not produce debugging output
     debug_level = debug.DebugLevel.ERROR
     console, _ = configure.setup(debug_level)
+    # define a URL that can be interacted with in a mocked fashion; importantly,
+    # this test case is using responses and thus there is no actual interaction
+    # with the GitHub API and no network transmission at all
     github_api_url = "https://api.github.com/repos/home-assistant/core/actions/runs"
+    # define the response that the mocked API will return for al interactions
     responses.add(
         responses.GET,
         github_api_url,
         json={"message": "server error"},
         status=502,
     )
+    # attempt to download the JSON data from the mocked GitHub API; since this
+    # will always result in failure this will cause the exponential back-off
+    # and retry mechanism to start working until the maximum number of retries
     (
         valid,
         total_retry_count,
