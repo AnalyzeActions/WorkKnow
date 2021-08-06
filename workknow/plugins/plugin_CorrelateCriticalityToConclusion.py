90584e291c0c01f752f8e47e033d01eae66d2188
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-08-06 19:27:30 -0400

    feat: Add basic group by in plugin_CorrelateCriticalityToConclusion.

--- workknow/plugins/plugin_CorrelateCriticalityToConclusion.py
+++ workknow/plugins/plugin_CorrelateCriticalityToConclusion.py
@@ -1,9 +1,13 @@
 """Plugin: Study the correlation between the project's criticality and its build conclusion."""
 
+import logging
+
 import pandas
 
 from typing import Tuple
 
+
+from workknow import constants
 from workknow import configure
 
 
@@ -13,7 +17,32 @@ def analyze(
     all_workflows_df: pandas.DataFrame,
 ) -> Tuple[pandas.DataFrame, str, bool]:
     """Plugin: Study the correlation between the project's criticality and its build conclusion."""
+    logger = logging.getLogger(constants.logging.Rich)
     console = configure.setup_console()
-    console.print("Running analysis")
+    logger.debug(f"All counts {all_counts_df}")
+    logger.debug(f"All commits {all_commits_df}")
+    logger.debug(f"All workflows {all_workflows_df}")
+    logger.debug(all_workflows_df.columns.tolist())
+    if "conclusion" in all_workflows_df:
+        dfkeep = all_workflows_df[["organization", "repo", "status", "conclusion"]]
+        logger.debug(dfkeep)
+        groupby = (
+            dfkeep
+            .groupby(["organization", "repo", "conclusion"], as_index=True)
+            .agg(["count"])
+        ).reset_index()
+        pandas.set_option("display.max_columns", None)
+        pandas.set_option("display.max_rows", None)
+        logger.debug(all_workflows_df)
+        logger.debug(groupby)
+        console.print()
+        console.print(groupby)
+        console.print(type(groupby))
+        if "conclusion" in groupby:
+            console.print("here")
+            console.print(groupby)
+            logger.debug(groupby.columns.tolist())
+        else:
+            console.print("not there")
     return_value_df = pandas.DataFrame()
     return (return_value_df, "stats", True)
