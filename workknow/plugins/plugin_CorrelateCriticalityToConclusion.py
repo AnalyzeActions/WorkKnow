133accec8b66b2353f923828ab8b64bd0ffb171d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-08-06 15:18:26 -0400

    feat: Add return type annot to analyze in plugin_CorrelateCriticalityToConclusion.

--- workknow/plugins/plugin_CorrelateCriticalityToConclusion.py
+++ workknow/plugins/plugin_CorrelateCriticalityToConclusion.py
@@ -2,6 +2,8 @@
 
 import pandas
 
+from typing import Tuple
+
 from workknow import configure
 
 
@@ -9,7 +11,7 @@ def analyze(
     all_counts_df: pandas.DataFrame,
     all_commits_df: pandas.DataFrame,
     all_workflows_df: pandas.DataFrame,
-):
+) -> Tuple[pandas.DataFrame, str, bool]:
     """Plugin: Study the correlation between the project's criticality and its build conclusion."""
     console = configure.setup_console()
     console.print("Running analysis")
