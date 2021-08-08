ae34b702649d9790111bd819bf76130347ccc711
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-08-08 11:51:45 -0400

    feat: Add analyze draft in plugin_CorrelateCriticalityToConclusion.

--- workknow/plugins/plugin_CorrelateCriticalityToConclusion.py
+++ workknow/plugins/plugin_CorrelateCriticalityToConclusion.py
@@ -23,13 +23,14 @@ def analyze(
     logger.debug(f"All commits {all_commits_df}")
     logger.debug(f"All workflows {all_workflows_df}")
     logger.debug(all_workflows_df.columns.tolist())
+    repo_dict_list = []
     if "conclusion" in all_workflows_df:
         dfkeep = all_workflows_df[["organization", "repo", "status", "conclusion"]]
         logger.debug(dfkeep)
         groupby = (
-            dfkeep
-            .groupby(["organization", "repo", "conclusion"], as_index=True)
-            .agg(["count"])
+            dfkeep.groupby(["organization", "repo", "conclusion"], as_index=True).agg(
+                ["count"]
+            )
         ).reset_index()
         pandas.set_option("display.max_columns", None)
         pandas.set_option("display.max_rows", None)
@@ -44,5 +45,39 @@ def analyze(
             logger.debug(groupby.columns.tolist())
         else:
             console.print("not there")
-    return_value_df = pandas.DataFrame()
+        repositories = groupby["repo"].unique()
+        logger.debug(repositories)
+        for repository in repositories:
+            repo_dict = {}
+            repo_group = groupby.loc[lambda df: df["repo"] == repository]
+            repo_group_failure = repo_group.loc[repo_group["conclusion"] == "failure"]
+            repo_group_failure_count = repo_group_failure["status"].values[0][0]
+            logger.debug(repo_group)
+            logger.debug(f"repo group failure: {repo_group_failure}")
+            logger.debug(f"*** failure count: {repo_group_failure_count}")
+            # repo_group_not_failure = repo_group.loc[
+            #     repo_group["conclusion"] != "failure"
+            # ]
+            repo_group_not_failure = repo_group[~repo_group.conclusion.isin(["failure"])]
+            if not repo_group_not_failure.empty:
+                repo_group_not_failure_count = groupby["status"].sum()
+            else:
+                repo_group_not_failure_count = 0
+            logger.debug(f"repo group not failure: {repo_group_not_failure}")
+            logger.debug(f"*** not failure count: {repo_group_not_failure_count}")
+            repo_dict["repo"] = repository
+            organization_name = repo_group["organization"].unique().tolist()[0]
+            logger.debug(f"org value: {organization_name}")
+            repo_dict["organization"] = organization_name
+            failure_percentage = (repo_group_failure_count) / (
+                repo_group_failure_count + repo_group_not_failure_count
+            )
+            logger.debug("calculated failure percentage: " + str(failure_percentage))
+            repo_dict["failure_percentage"] = failure_percentage
+            repo_dict_list.append(repo_dict)
+            # groupby.loc[(groupby["repo"] == repository), "percent_failure"] = 27
+        logger.debug(f"groupby modified = {groupby}")
+    return_value_df = pandas.DataFrame(repo_dict_list)
+    logger.debug(return_value_df)
+    console.print(return_value_df)
     return (return_value_df, "stats", True)
